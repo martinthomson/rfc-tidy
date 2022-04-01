4765cc8dba0f06bd5a23fc81af52c3ddb19e0a53
Martin Thomson <mt@lowentropy.net>
2022-04-01 12:00:33 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import io
+import os
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="rfc-tidy",
+        version="0.1.0",
+        author="Martin Thomson",
+        author_email="mt@lowentropy.net",
+        scripts=["rfc-tidy"],
+        packages=find_packages(),
+        description="Cleanup RFC XML",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+        ],
+        keywords="rfc ietf xml",
+        python_requires=">=3.6",
+        install_requires=[],
+        url="https://github.com/martinthomson/rfc-tidy",
+        license="MIT",
+    )
